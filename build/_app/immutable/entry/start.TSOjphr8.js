4317258013fd11054f6486a9fcd0a39b8c6ffa0b
antjoh1 <johman.antonio@gmail.com>
2025-02-02 23:10:18 +0100

    test

---
+++ build/_app/immutable/entry/start.TSOjphr8.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/Bca9zHpc.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
-			strict: true
+			strict: true,
+			trailingSlash: 'always'
 		}),
 
 		paths: {

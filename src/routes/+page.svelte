aabe01f7d9021e329412291051bb25cff7476e2a
antjoh1 <johman.antonio@gmail.com>
2025-02-03 10:59:13 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,8 @@
 </script>
 
 <div class='photoCarousel' >
-<PhotoPreview></PhotoPreview>
+    <PhotoPreview></PhotoPreview>
 </div>
-
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
 
 <style>
 

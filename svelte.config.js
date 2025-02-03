f23d3e0d1fc091d1050d10fb00c58513b05d3027
antjoh1 <johman.antonio@gmail.com>
2025-02-03 10:06:08 +0100

    thing

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,8 @@ const config = {
 		}),
 
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/5photos' : process.env.BASE_PATH,
+			// base: process.env.NODE_ENV === 'production' ? '/5photos' : process.env.BASE_PATH,
+			base: '/5photos'
 		}
 	},
 

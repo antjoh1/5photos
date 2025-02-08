7070386c2566806867784707e444b97042e80939
antjoh1 <johman.antonio@gmail.com>
2025-02-08 01:58:30 +0100

    build-edits

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,19 +5,17 @@
 </script>
 
 <header > 
-	
-	<div class='siteLogo'> 5 PHOTOS </div>
-	<div class="textContainer">
+	<div class='textContainer'> 5 PHOTOS </div>
+	<div class='textContainer interactive'>
         reach out
         <div class="circle"></div>
     </div>
-	
 </header>
 
 {@render children()}
 
 <footer>
-	<div class="textContainer bottom">
+	<div class="textContainer interactive bottom">
         archive
         <div class="circle"></div>
     </div>
@@ -28,33 +26,34 @@
 	@import url('https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap');
 	
 	header { 
+		width: 100vw;
 		display: flex; 
 		justify-content: space-between;
-		padding: 2rem;
-		margin-bottom: 1rem;
+		align-content:last baseline;
+		padding: 1rem 5rem 1rem 5rem;
 		background-color: var(--header-color);
 		color: var(--text-color-header);
 		font-family: 'Lexend exa';
 		font-weight: 200;
-		font-size: xx-large;
+		font-size: 1.5rem;
 	}
 
 	footer {
 		display: flex;
 		justify-content: space-between;
-		padding: 2rem;
-		background-color: var(--background-color-2);
+		padding: 1rem 5rem 2.5rem 5rem;
+		background-color: var(--background-color-3);
 		font-family: 'Lexend exa';
-		font-weight: 200;
-		font-size: x-large;
+		font-weight: 100;
+		font-size: 2rem;
 	}
 
 	.textContainer { 
 		position: relative;
 		display: inline-block;
-		/* background-color: black; */
 		color: white;
-		font-family: sans-serif;
+		font-family: 'Lexend exa';
+		font-weight: 100;
 		font-size: 2rem;
 		padding-right: 20px;
 		padding-left: 20px;
@@ -67,7 +66,7 @@
 	}
 
 	/* Hover effect */ 
-    .textContainer:hover {
+    .textContainer.interactive:hover {
            color: black; /* Text changes to black */
 		   z-index: 1;
     }
@@ -81,7 +80,7 @@
 		background-color: #F19AC8;
 		border-radius: 50%;
 		transition: all 250ms ease-in-out;
-     }
+    }
 
 	.textContainer:hover .circle {
             width: 100%;
@@ -91,6 +90,6 @@
             right: 0;
             background-color: #F19AC8;
             z-index: -1; 
-        }
+    }
 
 </style>

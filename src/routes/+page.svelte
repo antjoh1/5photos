8513bdcb74d1ed30bec668979908c4389216a3a0
antjoh1 <johman.antonio@gmail.com>
2025-02-10 20:31:53 +0100

    first load content and page transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,40 @@
 
     let blurBg = $state(false); 
     let introMsgFlag = $state(false);
-    let introDuration = 800;
+    let introDuration = 1000;
+    let loadCount = $state(0) // attempt at making this appear only on first load
 
     onMount( () => {
-        setTimeout(() => { 
-            blurBg  = true
-        }, introDuration);// This should match `out:fade` duration 
-        
-        setTimeout(() => { 
+        if (loadCount < 1){
+            blurBg = true
             introMsgFlag = true
-        }, introDuration*1.5);// This should match `out:fade` duration 
-        
+        }
+
     })
 
 </script>
 
 {#if blurBg}
-    <div class='firstLoadWindow' in:fade={{duration: 500}} out:fade={{duration: 500}}>
-        {#if introMsgFlag}
-            <div class='firstLoadMessage' in:fly={{x:-800, duration: 1500, easing:quartOut}} out:fly={{x:-800, duration: 500, easing:quartIn}}>
-                This is first load
-                <button onclick={()=>{setTimeout(() => { introMsgFlag = false}, introDuration); blurBg=false}}> Thanks! </button>
-            </div>
-        {/if}
+    <div class='firstLoadWindow' in:fade={{delay:500, duration: 500}} out:fade={{delay: 500, duration: 500}}></div>
+{/if}
+{#if introMsgFlag}
+    <div class='firstLoadMessage' in:fly={{delay: introDuration*1.5, x:-1000, duration: 2000, easing:quartOut}} out:fly={{x:-800, duration: 500, easing:quartIn}}>
+        <h1 class='firstLoadTitle'>Hi!</h1>
+        <h2>
+            This is a little page where I post 5 photos every week. 
+        </h2>
+        <h2> If you like any of the photos and you'd like to have them in higher quality
+            please reach out. I'd be happy to send them to you.
+        </h2>
+        <p class='disclaimerText'> If you find yourself on a photo and you don't want it to be online - please reach out and I'll take it down asap! </p>
+
+        <div class='firstLoadButton'>
+            <button  onclick={()=>{blurBg=false; introMsgFlag=false}}> 
+                Welcome!
+                <!-- <div class='circle'></div> -->
+             </button>
+            
+        </div>
     </div>
 {/if}
 
@@ -50,7 +61,10 @@
     }
 
     .firstLoadMessage { 
-        height: 30vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 40vh;
         width: 50vw;
         position: fixed;
         inset: 0px;
@@ -61,7 +75,40 @@
         border-radius: 10px;
         z-index: 3;
         padding: 3rem;
+        font-family: 'Lexend exa';
+        font-size: 1.5rem;
+        font-weight: 200;
     }
+
+    .disclaimerText { 
+        font-size: large;
+        font-style: italic;
+    }
+
+    .firstLoadButton { 
+        background-color: var(--pink-accent);
+        /* width: 40%; */
+        height: 3rem;
+        position: relative;
+        align-self: center;      
+        font-family: 'Lexend exa';
+        font-weight: 200;
+        font-size: 2rem;
+        padding-right: 20px;
+        padding-left: 20px;
+        border-radius: 5px;
+        transition: color 250ms ease-in-out;
+    }
+
+    .firstLoadButton:hover { 
+        background-color: #ee79b7;
+        transition: color 2500ms ease-in-out;
+    }
+    .firstLoadButton:active { 
+        transition: color 250ms ease-in-out;
+        background-color:  #ed6db1;
+    }
+
 
     .photoCarousel {
         margin-top: 2rem;

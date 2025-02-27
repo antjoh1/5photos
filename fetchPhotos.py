0dfea1fd2a7d4cdb4483ecc5912f22041de5f494
antjoh1 <johman.antonio@gmail.com>
2025-02-27 18:11:45 +0100

    thing

--- fetchPhotos.py
+++ fetchPhotos.py
@@ -22,7 +22,7 @@ root_response = requests.get(url).json()
 
 folders = [] 
 
-print(root_response)
+print(root_response, API_KEY)
 
 
 ## ----- Create folders and download files into Static/ ----- ## 

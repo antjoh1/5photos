77161e68c79e0e6520532c653edfc9dcd395942f
antjoh1 <johman.antonio@gmail.com>
2025-04-04 01:02:19 +0200

    made first db tests

--- backend/crud/images.py
+++ backend/crud/images.py
@@ -1,6 +1,15 @@
-from models.images import Image
+from models.images import Image, Images
 from sqlmodel import Session
 
+def get_one_image(imgLoc: str | None, ordinalNum: str | None, session: Session) -> str: 
+    return f"get_one_image() ran, {imgLoc}, {ordinalNum}" 
 
-def get_one_image(imgLoc: str, ordinalNum: str, session: Session) -> Image: 
-    return "get_one_image() ran"
+
+def add_one_image(image: Image, session: Session) -> Image:
+    newEntry = Images(path=image.path, ordinalNum=image.ordinalNum, imgLocation=image.imgLocation, altText=image.altText)
+
+    print(image.model_dump())
+    session.add(newEntry)
+    session.commit()
+    session.refresh(newEntry)
+    return image

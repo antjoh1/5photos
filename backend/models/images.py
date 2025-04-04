adbebd777fb238ce1919d3d20e7bda977ea1b7c4
antjoh1 <johman.antonio@gmail.com>
2025-04-04 11:10:50 +0200

    initial routing work started

--- backend/models/images.py
+++ backend/models/images.py
@@ -5,8 +5,13 @@ class Image(SQLModel):
     imgLocation: str
     ordinalNum: str
     altText: str
+    rating: int = 0
     
 class Images(Image, table = True): 
     id: int = Field(None, primary_key=True)
 
-
+class ImageUpdate(Image):
+    path: str | None = None 
+    ordinalNum: str | None = None
+    altText: str | None = None
+    rating: int | None = None 
